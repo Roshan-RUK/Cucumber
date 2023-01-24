77fc991bfd92896b2ca452118c24b95cd1eac4b6
Roshan-RUK <roshanullakhan@gmail.com>
2023-01-24 13:38:34 +0530

    Updated step definition file

--- src/CucumberTesting/stepDefinitions/stepDefinitionImplimentation.java
+++ src/CucumberTesting/stepDefinitions/stepDefinitionImplimentation.java
@@ -1,7 +1,10 @@
 package CucumberTesting.stepDefinitions;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class stepDefinitionImplimentation {
 	@Given("I landed on Ecommerce page")
@@ -9,6 +12,31 @@ public class stepDefinitionImplimentation {
 	{
 System.setProperty("webdriver.chrome.driver","C://CHromeDriverJava/chromedriver.exe");
 ChromeDriver driver = new ChromeDriver();
+System.out.println("Inside Background, first execution");
 
+	}
+	
+	@Given("^Logged in with username (.+) and Password (.+)$")
+	public void Logged_in_with_username_and_Password(String username,String password)
+	{
+		System.out.println("The username is "+username+"Password is"+password);
+	}
+	
+	@When("^I add product (.+) from cart$")
+	public void I_add_product_from_cart(String productName)
+	{
+		System.out.println("The product name is"+productName);
+	}
+	
+	@And("^CHeckout (.+) and submit the order$")
+	public void CHeckout_and_submit_the_order(String productName)
+	{
+		System.out.println("The product name is"+productName);
+	}
+	
+	@Then("{string} message is displayed on confirmationpage")
+	public void message_is_displayed_on_confirmationpage(String message)
+	{
+		System.out.println("the Message is"+message);
 	}
 }
